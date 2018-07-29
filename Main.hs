baa40aae064e556ff3664676409e5491ef487830
omersagy <omersagyemails@gmail.com>
2018-07-29 18:54:41 -0400

    remove dead code.

--- Main.hs
+++ Main.hs
@@ -11,31 +11,6 @@ import qualified Data.Map as Map
 import Data.Monoid
 
 
-data Currency = USD | EUR
-
-data Money = Money Int Currency
-
-instance Show Money where
-
-  show :: Money -> String
-  show (Money val currency) =
-    case currency of
-      USD -> "$" ++ (show val)
-      EUR -> "€" ++ (show val)
-
-
-doubleMoney :: Money -> Money
-doubleMoney (Money val currency) = (Money (val*2) currency)
-
-
-dollars :: Int -> Money
-dollars x = Money x USD
-
-
-euros :: Int -> Money
-euros x = Money x EUR
-
-
 data Message = Inc 
                | Dec
                | Zero
@@ -79,34 +54,7 @@ main = mainWidget root
 
 root :: forall t m. MonadWidget t m => m ()
 root = mdo
-  clickEvent <- button "Click Me!" 
-  tot <- count clickEvent
-  tot2 <- doubleCount clickEvent
-  el "div" (display tot)
-  el "div" (display tot2)
-  el "div" blank
-  mapM_ (el "div") (map text ["hello", "world"])
 
-  -- Main loop.
   model <- foldDyn update initModel message
   message <- render model
-
   return ()
-
-
-doubleCount :: forall t m a. MonadWidget t m =>
-               Event t a -> m (Dynamic t Int)
-doubleCount eventCounter = foldDyn doubleCounter 0 (1 <$ eventCounter)
-
-
-doubleCounter :: Int -> Int -> Int
-doubleCounter x accumulator =
-  let
-    g = h
-    h = 2
-  in
-    accumulator + g * x
-
-
-toText :: String -> T.Text
-toText s = T.pack s
